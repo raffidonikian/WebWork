95cf1e5054ef05f28b7963100c74c2f1eb666f61
aml <dirk@amls-MacBook-Pro.local>
2018-08-14 12:39:10 -0700

    Backend Completed

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://raffi:Horse94jump!@ds119442.mlab.com:19442/diran"
-}
+  mongoURI: 'mongodb://raffi:avedikian1@ds119442.mlab.com:19442/diran',
+  secretOrKey: 'secret'
+};
--- server.js
+++ server.js
@@ -1,39 +1,38 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
-
-
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-
-
-
-
-
-//db config
+// DB Config
 const db = require('./config/keys').mongoURI;
-//connect to mongodb
+
+// Connect to MongoDB
 mongoose
   .connect(db)
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
+// Passport middleware
+app.use(passport.initialize());
 
+// Passport Config
+require('./config/passport')(passport);
 
-
-
-app.get('/', (req, res) => res.send("hello nodemon"));
-
-//Use routes
+// Use Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
+const port = process.env.PORT || 5000;
 
-const port = process.env.Port || 5000;
-
-app.listen(port, () => console.log('Server running on port $(port)'));
+app.listen(port, () => console.log(`Server running on port ${port}`));
